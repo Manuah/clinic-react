e648f0922b8c97e82952cc5784d68a02e2855141
Zlata <muuakhina.zlata@gmail.com>
2024-05-25 21:02:27 +0300

    um yeah jwt check failed

--- src/privateRouter.tsx
+++ src/privateRouter.tsx
@@ -5,11 +5,27 @@ Component: React.ComponentType,
 role: string
 }
 
+// async function checkToken(token: string | null) {
+//     const response = await fetch('http://localhost:5000/auth/checkTokenValidate?token=' + token, {
+//      }) 
+     
+//     const data = await response.json();
+//     //alert(JSON.stringify(data));
+//     if (response.ok)
+//     {
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+//   }
+
 export const PrivateRoute = ({
     Component, role
 }: PrivateRouterProps): ReactElement => {
     const location = useLocation();
     const isAuthenticated = !!localStorage.getItem("token");
+    //const isTokenValid = awaitcheckToken(localStorage.getItem("token"))
     const IsRoleValid = localStorage.getItem("roleId") == role;
     // const [IsRoleValid, setIsRoleValid]= useState(false);
     // if (localStorage.getItem("roleId") == role)
@@ -21,7 +37,7 @@ export const PrivateRoute = ({
     //     setIsRoleValid(false);
     // }
     
-return isAuthenticated && IsRoleValid ? (
+return  isAuthenticated && IsRoleValid ? (
     <Component/>
     ) : (
         <Navigate to="/" state={{from: location}}/> );
